cb5471ed86f1ea60a5ff621b50f9ede37b2d0a9e
khaledk2 <khaledk2@hotmail.com>
2025-11-20 13:10:15 +0000

    debug

--- 99-run.sh
+++ 99-run.sh
@@ -6,5 +6,6 @@ omero=/opt/omero/server/venv3/bin/omero
 cd /opt/omero/server
 echo "test from here..."
 echo "Starting OMERO.server"
-exec $omero admin start --foreground
+echo "$omero"
+echo exec $omero admin start --foreground
 echo "DONE !"
