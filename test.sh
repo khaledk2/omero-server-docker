57bb24cfacb362371edc7bddd30b975c1edb255f
khaledk2 <khaledk2@hotmail.com>
2025-11-20 11:42:15 +0000

    test

--- test.sh
+++ test.sh
@@ -35,7 +35,7 @@ docker run -d --name $PREFIX-server --link $PREFIX-db:db \
     $IMAGE
 
 # Smoke tests
-docker logs -f $PREFIX-server
+docker ps
 export OMERO_USER=root
 export OMERO_PASS=omero-root-password
 export PREFIX
