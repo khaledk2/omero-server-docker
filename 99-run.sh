06ba8b205b44b9a57c87db32f34ae39dada9090c
khaledk2 <khaledk2@hotmail.com>
2025-11-22 19:42:55 +0000

    Fix

--- 99-run.sh
+++ 99-run.sh
@@ -2,7 +2,9 @@
 
 set -eu
 
-omero=/opt/omero/server/venv3/bin/omero
+# omero=/opt/omero/server/venv3/bin/omero
+omero=/opt/omero/server/OMERO.server/bin/omero
+
 cd /opt/omero/server
 echo "Starting OMERO.server"
 exec $omero admin start --foreground
